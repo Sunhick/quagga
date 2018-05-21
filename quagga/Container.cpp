50e245bb1c5fc70046e3acf22c875e98f369312c
Sunil <sunhick@gmail.com>
2018-05-20 23:49:36 -0700

    support lazy evaluations of registrations

--- quagga/Container.cpp
+++ quagga/Container.cpp
@@ -1,5 +1,33 @@
 //  Copyright © 2018 Sunil. All rights reserved.
 
+#include <sstream>
+
+#ifdef __GNUG__
+#include <cxxabi.h>
+#include <cstdlib>
+#include <memory>
+
+struct handle {
+  char* p;
+  handle(char* ptr) : p(ptr) {}
+  ~handle() { std::free(p); }
+};
+
+std::string demangle(const char* name) {
+  int status = -4;
+  handle result(abi::__cxa_demangle(name, NULL, NULL, &status));
+  return (status == 0) ? result.p : name;
+}
+
+#else
+
+// does nothing if not g++
+std::string demangle(const char* name) {
+  return name;
+}
+
+#endif
+
 namespace quagga {
 
 template <class T>
@@ -9,13 +37,30 @@ void Container::Register(std::shared_ptr<Component> instance) {
 
 template <class T>
 std::shared_ptr<T> Container::Resolve() {
-  auto instance = instances[typeid(T)];
-  return std::dynamic_pointer_cast<T>(instance);
+  const auto& index = typeid(T);
+  if (auto found = instances.find(index); found != instances.end()) {
+    // found a valid instance registered.
+    // auto instance = instances[typeid(T)];
+    return std::dynamic_pointer_cast<T>(found->second);
+  }
+
+  if (auto registration = registrations.find(index);
+      registration != registrations.end()) {
+    // found a registration for creating the instance
+    auto instance = registration->second();
+    instances[index] = instance;
+    return std::dynamic_pointer_cast<T>(instance);
+  }
+
+  std::ostringstream stream;
+  stream << "unable to resolve the type: " << demangle(typeid(T).name());
+  std::string error = stream.str();
+  throw std::invalid_argument(error);
 }
 
 template <typename T>
 void Container::Register(ComponentMaker creator) {
-  // No op for now
+  registrations[typeid(T)] = creator;
 }
 
 }  // namespace quagga
--- quaggaTests/main.cpp
+++ quaggaTests/main.cpp
@@ -7,10 +7,37 @@
 
 class Foo : public quagga::Component {
 public:
+    ~Foo() {
+        std::cout << "Foo destroyed" << std::endl;
+    }
+    
     void print() {
         std::cout << "Foo -- print function" << std::endl;
     }
 };
+
+class Bar : public quagga::Component {
+public:
+    ~Bar() {
+        std::cout << "Bar destroyed" << std::endl;
+    }
+    
+    void print() {
+        std::cout << "Bar -- print function" << std::endl;
+    }
+};
+
+class FooBar : public quagga::Component {
+public:
+    ~FooBar() {
+        std::cout << "FooBar destroyed" << std::endl;
+    }
+    
+    void print() {
+        std::cout << "FooBar -- print function" << std::endl;
+    }
+};
+
 
 int main(int argc, const char * argv[]) {
     quagga::Container container;
@@ -21,9 +48,14 @@ int main(int argc, const char * argv[]) {
     auto copyFoo = container.Resolve<Foo>();
     copyFoo->print();
     
-    container.Register<Foo>([]() {
-        return std::make_shared<Foo>();
+    container.Register<Bar>([]() {
+        return std::make_shared<Bar>();
     });
+    
+    auto copyBar = container.Resolve<Bar>();
+    copyBar->print();
+    
+    auto error = container.Resolve<FooBar>();
     
     std::cout << "Hello World!" << std::endl;
     return 0;
